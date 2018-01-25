804af05e51389fb0b6d2f36edb17d339b6bc036f
GuillaumeRZ <gui.remyzephir@gmail.com>
2018-01-25 17:01:20 +0100

    Oupsi, forgot post.save

--- tp/apps/post.py
+++ tp/apps/post.py
@@ -7,4 +7,5 @@ app_post = Blueprint('app_post', __name__)
 @app_post.route('/posts', methods=['GET', 'POST'])
 def app():
   post = Post.create(title='lol', description='la bonne description')
+  post.save()
   return 'post'
