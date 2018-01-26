ef9c98c3601dc61048b87b888eeec391b39e6418
GuillaumeRZ <gui.remyzephir@gmail.com>
2018-01-26 10:54:46 +0100

    Add get post/<id> method

--- tp/apps/post.py
+++ tp/apps/post.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, jsonify, request
 from helpers.post_helper import post_helper
 from models import Post
+from playhouse.shortcuts import model_to_dict, dict_to_model
 
 app_post = Blueprint('app_post', __name__)
 
@@ -14,5 +15,17 @@ def create_post():
   post = Post.create(title=title, description=description)
   post.save()
 
-  # return jsonify({'data': list(Post.select().dicts()) }), 201
-  return 'test'
+  # Convert our model (object) to dictionnary
+  data = model_to_dict(post)
+  # Jsonify work only with dict, we've to convert everything
+  return jsonify({'data': data}), 201
+
+# Create article
+@app_post.route('/post/<id>', methods=['GET'])
+def get_post(id):
+  post = Post.get(Post.id == id)
+  # We convert once again
+  data = model_to_dict(post)
+
+  # We send the information to the browser
+  return jsonify({'data': data}), 201
