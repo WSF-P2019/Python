2d18a0a0a7bd0b6fd9a8a898ca1e045148fbbb83
GuillaumeRZ <gui.remyzephir@gmail.com>
2018-01-26 10:32:11 +0100

    Add post route

--- tp/apps/post.py
+++ tp/apps/post.py
@@ -1,11 +1,19 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
 from helpers.post_helper import post_helper
 from models import Post
 
 app_post = Blueprint('app_post', __name__)
 
-@app_post.route('/posts', methods=['GET', 'POST'])
-def app():
-  post = Post.create(title='lol', description='la bonne description')
-  post.save()
-  return jsonify({'data': list(Post.select().dicts()) }), 201
+# Create article
+@app_post.route('/posts', methods=['POST'])
+def create_post():
+  params = request.get_json() # dict
+  title = params.get('title')
+  description = params.get('description')
+  print title, description
+
+  # post = Post.create(title='lol', description='la bonne description')
+  # post.save()
+
+  # return jsonify({'data': list(Post.select().dicts()) }), 201
+  return 'test'
